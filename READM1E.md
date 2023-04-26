88961a7fc06595f8aac2f868bd233a00eb52b45b
letengzz <2020885569@qq.com>
2023-04-26 11:17:20 +0800

    y''

---
+++ READM1E.md
@@ -0,0 +1,5 @@
+<div>
+  <img height="170" width="500" align="left" src="https://github-readme-stats.vercel.app/api?username=letengzz&count_private=true&include_all_commits=true&&show_icons=true&theme=blueberry" />
+  <img height="173" src="https://github-readme-stats.vercel.app/api/top-langs/?username=letengzz&layout=compact" />
+</div>
+<div align="center"><img src="https://raw.githubusercontent.com/letengzz/letengzz/main/assets/github-contribution-grid-snake.svg" /></div>
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar =
+      '█'.repeat(passedProgressBarIndex) +
+      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
+    return `{ ${progressBar} }`
+}
+
+const readme = `\
 ### Hi there 👋
 
-⏳ Year progress { █████████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ } 31.51 %
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
 
-⏰ Updated on Wed, 26 Apr 2023 00:36:50 GMT
+⏰ Updated on ${new Date().toUTCString()}
 
 ---
 ### My GitHub Contributions    
@@ -28,4 +44,7 @@ I'm angola, a new-media editor.
 ![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-007ACC?style=flat-square&logo=Visual-Studio-Code&logoColor=white)       
 ![MWeb](https://img.shields.io/badge/%E5%9B%BE%E5%BA%8A-MWeb-lightgrey)          
 ![Git](https://img.shields.io/badge/-Git-black?style=plastic&logo=git)     
+\
+`
 
+console.log(readme)
